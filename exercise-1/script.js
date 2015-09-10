6c359416d64cedd80c549701abf83e6469434cc6
willis-meyers <s.willismeyers@gmail.com>
2015-09-09 21:20:21 -0700

    week 5 commit

--- exercise-1/script.js
+++ exercise-1/script.js
@@ -1,5 +1,14 @@
-/*
+var field = document.querySelector('#newitem');
+var form = document.querySelector('form');
+var todo = document.querySelector('#todolist');
+var check = document.querySelector('#check');
 
-  Your Javascript here
 
-*/
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    todo.innerHTML += '<li>' + field.value + '</li>';
+    field.value = ''+ check;
+    field.focus();
+
+    }
+)
